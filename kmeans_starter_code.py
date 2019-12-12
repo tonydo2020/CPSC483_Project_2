7927f16152c0ecb7695b75b2556513087e44a255
tonydo2020 <55472788+tonydo2020@users.noreply.github.com>
2019-12-11 22:04:09 -0800

    Update kmeans_starter_code.py

--- kmeans_starter_code.py
+++ kmeans_starter_code.py
@@ -4,6 +4,11 @@ Created on Thu Nov 14 00:45:00 2019
 
 @author: shlakhanpal
 """
+
+#CPSC 483
+#Tony Do
+#Sami Halwani
+
 import scipy.io
 import numpy as np
 import matplotlib.pyplot as plt
@@ -15,11 +20,12 @@ def distanceBetPointsAndCentroids(Y): # Y = [3, 300]
             dist[i,j] = np.linalg.norm(initial_centroids[i] - X[j])
             Y[i,j] = dist[i,j]
 
-
+def return_closest(Y):
+    min_values = (np.argmin(Y, axis = 0 ))
+    return min_values
 
 def return_best(Y): # Y, best_value = [3, 300]
     min_values = (np.argmin(Y, axis = 0 )) # [1, 300]
-    
     avg_X = 0
     avg_Y = 0
     for i in range(K):
@@ -42,17 +48,57 @@ def return_best(Y): # Y, best_value = [3, 300]
     #print(best_value)
 
     return initial_centroids
-
-def display_centroids(final_centroids, X, data):
-    data = data['X']
-    print(data)
-    x, y = zip(*data)
-    print(x)
-    print(y)
-    plt.scatter(x,y,)
-    plt.show()
     
+def logic(data,Y):
+    fig, graph = plt.subplots(3)
+    fig.suptitle("Project 2")
+    X = data['X']
+    x, y = zip(*X)
+    
+    graph[0].scatter(x,y, c = 'b')
+    graph[0].plot(initial_centroids[0:, 0], initial_centroids[0:, 1], 'yx')
+    graph[0].plot(initial_centroids[1:, 0], initial_centroids[1:, 1], 'rx')
+    graph[0].plot(initial_centroids[2:, 0], initial_centroids[2:, 1], 'cx')
 
+    distanceBetPointsAndCentroids(Y)
+    initial_iteration_centroids = return_best(Y)
+    min_values = return_closest(Y)
+
+    for a in range(len(min_values)):
+        if min_values[a] == 0:
+            graph[1].scatter(x[a],y[a], c = 'y')
+        elif min_values[a] == 1:
+            graph[1].scatter(x[a],y[a], c = 'r')
+        elif min_values[a] == 2:
+            graph[1].scatter(x[a],y[a], c = 'c')
+    
+    graph[1].plot(initial_iteration_centroids[0:, 0], initial_iteration_centroids[0:, 1], 'yx')
+    graph[1].plot(initial_iteration_centroids[1:, 0], initial_iteration_centroids[1:, 1], 'rx')
+    graph[1].plot(initial_iteration_centroids[2:, 0], initial_iteration_centroids[2:, 1], 'cx')
+
+
+    for b in range(9):
+        distanceBetPointsAndCentroids(Y)
+        final_iteration_centroids = return_best(Y)
+        print("at iteration ", b + 1, "centroid values are: ", final_iteration_centroids)
+
+    min_values = return_closest(Y)
+
+    for a in range(len(min_values)):
+        if min_values[a] == 0:
+            graph[2].scatter(x[a],y[a], c = 'y')
+        elif min_values[a] == 1:
+            graph[2].scatter(x[a],y[a], c = 'r')
+        elif min_values[a] == 2:
+            graph[2].scatter(x[a],y[a], c = 'c')
+    
+    graph[2].plot(final_iteration_centroids[0:, 0], final_iteration_centroids[0:, 1], 'yx')
+    graph[2].plot(final_iteration_centroids[1:, 0], final_iteration_centroids[1:, 1], 'rx')
+    graph[2].plot(final_iteration_centroids[2:, 0], final_iteration_centroids[2:, 1], 'cx')
+
+    plt.show()
+
+    
 datafile = 'kmeansdata.mat'
 data = scipy.io.loadmat(datafile)
 
@@ -60,6 +106,7 @@ data = scipy.io.loadmat(datafile)
 X = np.array([[1,1],[2,1],[4,3],[5,4]]) # class activity
 
 X = data['X']
+print("len of x is: ", len(X))
 
 num_points = len(X)
 
@@ -68,18 +115,8 @@ K = 3 # class activity
 initial_centroids = np.array([[3.0,3.0], [6.0,2.0], [8.0,5.0]])
 print("initial_centroids is : ", initial_centroids)
 
-#plt.plot(X[:, 0], X[:, 1], 'go')
-#plt.plot(initial_centroids[:, 0], initial_centroids[:, 1], 'rx')
-#plt.show()
-
 dist = np.zeros((K,num_points)) # rows by columns / centroid by data
 
 Y = np.zeros((K,num_points)) # 3 by 300
 
-for a in range(10):
-    distanceBetPointsAndCentroids(Y)
-    final_centroids = return_best(Y)
-
-print("final centroids are: ", final_centroids)
-
-display_centroids(final_centroids, X, data)
+logic(data,Y)
